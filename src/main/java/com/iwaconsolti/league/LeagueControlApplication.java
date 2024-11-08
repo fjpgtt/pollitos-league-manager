4009226352fc31c0c9a79ce81250cc24cd008de3
Francisco J Perez Gallardo <78174671+fjpgtt@users.noreply.github.com>
2024-11-08 13:18:49 -0600

    Initi commit

---
+++ src/main/java/com/iwaconsolti/league/LeagueControlApplication.java
@@ -0,0 +1,13 @@
+package com.iwaconsolti.league;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeagueControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeagueControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iwaconsolti/league/LeagueControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iwaconsolti.league;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeagueControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
